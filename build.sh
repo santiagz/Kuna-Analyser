9a7f4a8db2144f49bfa9c4cb1583280abca8231d
Yaroslav Ziabko <yaroslav.ziabko@stv.group>
2021-12-28 10:25:39 +0200

    change docker image settings

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 docker stop kuna-analyser && docker image rm kuna-analyser
 
-docker build -t kuna-analyser .
+docker build -t kuna-analyser . --no-cache
 
 docker run -it -d --name kuna --hostname kuna-an --rm kuna-analyser
